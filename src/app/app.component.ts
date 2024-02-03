3e4118f96fbf9877cb3c965487b8b939a58768c0
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-02-03 22:40:41 +0100

    Changing title to playerName & modif title of page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-learning-project';
+  playerName = 'Arnaud';
 }
